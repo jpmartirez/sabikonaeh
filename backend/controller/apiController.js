a3ad3ce006b31590f18ce6462f743bb86a6a4803
jpmartirez <johnpaulmartirez0915@gmail.com>
2025-12-31 21:20:23 +0800

    add try and catch block in api

--- backend/controller/apiController.js
+++ backend/controller/apiController.js
@@ -6,14 +6,18 @@ import { GoogleGenAI } from '@google/genai';
 export const api = async (req, res) => {
     const {prompt} = req.body;
 
-    const client = new GoogleGenAI({});
+    try {
+        const client = new GoogleGenAI({});
 
-    const interaction =  await client.interactions.create({
-        model: 'gemini-3-flash-preview',
-        input: prompt,
-    });
+        const interaction =  await client.interactions.create({
+            model: 'gemini-3-flash-preview',
+            input: prompt,
+        });
 
-    const result = interaction.outputs[interaction.outputs.length - 1].text;
+        const result = interaction.outputs[interaction.outputs.length - 1].text;
 
-    res.json({success: true, result})
+        res.json({success: true, result})
+    } catch (error) {
+        res.json({success: false, message: error.message})
+    }
 }
