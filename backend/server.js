44f4b0ac61585386696e3456e803ff3083d6fa4d
jpmartirez <johnpaulmartirez0915@gmail.com>
2025-12-31 19:34:40 +0800

    add vercel json

--- backend/server.js
+++ backend/server.js
@@ -5,11 +5,15 @@ import apiRouter from './routes/apiRoute.js';
 import cors from 'cors'
 
 const app = express();
+
+
 app.use(express.json())
 app.use(cors())
 
+const PORT = process.env.PORT || 3000;
+
 app.use('/api/openai', apiRouter)
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log("Server is listening to port 3000")
 })
