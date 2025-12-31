1b6ccc9252f87a195bdf67d78bed03f5d0d3eca9
jpmartirez <johnpaulmartirez0915@gmail.com>
2025-12-31 20:31:29 +0800

    fix text sizing

--- client/src/components/animation/TrueFocus.tsx
+++ client/src/components/animation/TrueFocus.tsx
@@ -91,7 +91,7 @@ const TrueFocus: React.FC<TrueFocusProps> = ({
             ref={el => {
               wordRefs.current[index] = el;
             }}
-            className="relative text-[3rem] font-black cursor-pointer"
+            className="relative text-2xl md:text-[2rem] lg:text-[3rem] font-black cursor-pointer"
             style={
               {
                 filter: manualMode
